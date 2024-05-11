0a2311ea83f7d178e95ac79332381f253c063a29
umersanii <iamumersani@gmail.com>
2024-05-12 02:34:57 +0500

    0.1.3
    Add Trainer

--- Gym/O_AddTrainer.Designer.cs
+++ Gym/O_AddTrainer.Designer.cs
@@ -28,20 +28,350 @@
         /// </summary>
         private void InitializeComponent()
         {
+            fname = new TextBox();
+            lname = new TextBox();
+            Dob = new TextBox();
+            textBox3 = new TextBox();
+            comboBox4 = new ComboBox();
+            textBox4 = new TextBox();
+            txtusername = new TextBox();
+            txtconfirmpass = new TextBox();
+            txtpass = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            DeleteB = new Button();
             SuspendLayout();
             // 
-            // FrmAddTrainer
+            // fname
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            fname.BackColor = Color.FromArgb(255, 255, 192);
+            fname.BorderStyle = BorderStyle.None;
+            fname.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            fname.Location = new Point(165, 106);
+            fname.Margin = new Padding(2);
+            fname.Multiline = true;
+            fname.Name = "fname";
+            fname.Size = new Size(223, 28);
+            fname.TabIndex = 54;
+            // 
+            // lname
+            // 
+            lname.BackColor = Color.FromArgb(255, 255, 192);
+            lname.BorderStyle = BorderStyle.None;
+            lname.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lname.Location = new Point(737, 106);
+            lname.Margin = new Padding(2);
+            lname.Multiline = true;
+            lname.Name = "lname";
+            lname.Size = new Size(223, 28);
+            lname.TabIndex = 55;
+            // 
+            // Dob
+            // 
+            Dob.BackColor = Color.FromArgb(255, 255, 192);
+            Dob.BorderStyle = BorderStyle.None;
+            Dob.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Dob.Location = new Point(165, 247);
+            Dob.Margin = new Padding(2);
+            Dob.Multiline = true;
+            Dob.Name = "Dob";
+            Dob.Size = new Size(223, 28);
+            Dob.TabIndex = 56;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = Color.FromArgb(255, 255, 192);
+            textBox3.BorderStyle = BorderStyle.None;
+            textBox3.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox3.Location = new Point(454, 247);
+            textBox3.Margin = new Padding(2);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(223, 28);
+            textBox3.TabIndex = 57;
+            // 
+            // comboBox4
+            // 
+            comboBox4.BackColor = Color.FromArgb(255, 255, 192);
+            comboBox4.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox4.FlatStyle = FlatStyle.Flat;
+            comboBox4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            comboBox4.FormattingEnabled = true;
+            comboBox4.Location = new Point(165, 330);
+            comboBox4.Margin = new Padding(2);
+            comboBox4.Name = "comboBox4";
+            comboBox4.Size = new Size(223, 29);
+            comboBox4.TabIndex = 58;
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = Color.FromArgb(255, 255, 192);
+            textBox4.BorderStyle = BorderStyle.None;
+            textBox4.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox4.Location = new Point(737, 247);
+            textBox4.Margin = new Padding(2);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(223, 28);
+            textBox4.TabIndex = 59;
+            // 
+            // txtusername
+            // 
+            txtusername.BackColor = Color.FromArgb(255, 255, 192);
+            txtusername.BorderStyle = BorderStyle.None;
+            txtusername.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtusername.Location = new Point(165, 471);
+            txtusername.Margin = new Padding(2);
+            txtusername.Multiline = true;
+            txtusername.Name = "txtusername";
+            txtusername.Size = new Size(223, 28);
+            txtusername.TabIndex = 60;
+            // 
+            // txtconfirmpass
+            // 
+            txtconfirmpass.BackColor = Color.FromArgb(255, 255, 192);
+            txtconfirmpass.BorderStyle = BorderStyle.None;
+            txtconfirmpass.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtconfirmpass.Location = new Point(737, 471);
+            txtconfirmpass.Margin = new Padding(2);
+            txtconfirmpass.Multiline = true;
+            txtconfirmpass.Name = "txtconfirmpass";
+            txtconfirmpass.Size = new Size(223, 28);
+            txtconfirmpass.TabIndex = 61;
+            // 
+            // txtpass
+            // 
+            txtpass.BackColor = Color.FromArgb(255, 255, 192);
+            txtpass.BorderStyle = BorderStyle.None;
+            txtpass.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtpass.Location = new Point(454, 471);
+            txtpass.Margin = new Padding(2);
+            txtpass.Multiline = true;
+            txtpass.Name = "txtpass";
+            txtpass.Size = new Size(223, 28);
+            txtpass.TabIndex = 62;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = Color.Black;
+            label2.Location = new Point(165, 146);
+            label2.Margin = new Padding(2, 0, 2, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(64, 15);
+            label2.TabIndex = 63;
+            label2.Text = "First Name";
+            label2.Click += label2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(737, 146);
+            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(63, 15);
+            label1.TabIndex = 64;
+            label1.Text = "Last Name";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.ForeColor = Color.Black;
+            label3.Location = new Point(165, 286);
+            label3.Margin = new Padding(2, 0, 2, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(110, 15);
+            label3.TabIndex = 65;
+            label3.Text = "DoB (yyyy-mm-dd)";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.ForeColor = Color.Black;
+            label4.Location = new Point(454, 286);
+            label4.Margin = new Padding(2, 0, 2, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(41, 15);
+            label4.TabIndex = 66;
+            label4.Text = "Rating";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.ForeColor = Color.Black;
+            label5.Location = new Point(165, 371);
+            label5.Margin = new Padding(2, 0, 2, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(32, 15);
+            label5.TabIndex = 67;
+            label5.Text = "Gym";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.ForeColor = Color.Black;
+            label6.Location = new Point(737, 286);
+            label6.Margin = new Padding(2, 0, 2, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(108, 15);
+            label6.TabIndex = 68;
+            label6.Text = "Years of Experience";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.Black;
+            label7.Location = new Point(165, 70);
+            label7.Margin = new Padding(2, 0, 2, 0);
+            label7.Name = "label7";
+            label7.Size = new Size(64, 25);
+            label7.TabIndex = 69;
+            label7.Text = "Name";
+            label7.Click += label7_Click;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.ForeColor = Color.Black;
+            label8.Location = new Point(165, 213);
+            label8.Margin = new Padding(2, 0, 2, 0);
+            label8.Name = "label8";
+            label8.Size = new Size(152, 25);
+            label8.TabIndex = 70;
+            label8.Text = "Personal Details";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.ForeColor = Color.Black;
+            label9.Location = new Point(165, 437);
+            label9.Margin = new Padding(2, 0, 2, 0);
+            label9.Name = "label9";
+            label9.Size = new Size(190, 25);
+            label9.TabIndex = 71;
+            label9.Text = "Account Credentials";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.ForeColor = Color.Black;
+            label10.Location = new Point(165, 510);
+            label10.Margin = new Padding(2, 0, 2, 0);
+            label10.Name = "label10";
+            label10.Size = new Size(60, 15);
+            label10.TabIndex = 72;
+            label10.Text = "Username";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.ForeColor = Color.Black;
+            label11.Location = new Point(454, 510);
+            label11.Margin = new Padding(2, 0, 2, 0);
+            label11.Name = "label11";
+            label11.Size = new Size(57, 15);
+            label11.TabIndex = 73;
+            label11.Text = "Password";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.ForeColor = Color.Black;
+            label12.Location = new Point(737, 510);
+            label12.Margin = new Padding(2, 0, 2, 0);
+            label12.Name = "label12";
+            label12.Size = new Size(104, 15);
+            label12.TabIndex = 74;
+            label12.Text = "Confirm Password";
+            // 
+            // DeleteB
+            // 
+            DeleteB.BackColor = Color.Black;
+            DeleteB.Cursor = Cursors.Hand;
+            DeleteB.FlatStyle = FlatStyle.Flat;
+            DeleteB.ForeColor = Color.Gold;
+            DeleteB.Location = new Point(496, 591);
+            DeleteB.Margin = new Padding(2);
+            DeleteB.Name = "DeleteB";
+            DeleteB.Size = new Size(132, 43);
+            DeleteB.TabIndex = 75;
+            DeleteB.Text = "Confirm";
+            DeleteB.UseVisualStyleBackColor = false;
+            DeleteB.Click += DeleteB_Click;
+            // 
+            // O_AddTrainer
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(46, 51, 73);
-            ClientSize = new Size(800, 450);
+            BackColor = Color.Gold;
+            ClientSize = new Size(1160, 700);
+            Controls.Add(DeleteB);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Controls.Add(label2);
+            Controls.Add(txtpass);
+            Controls.Add(txtconfirmpass);
+            Controls.Add(txtusername);
+            Controls.Add(textBox4);
+            Controls.Add(comboBox4);
+            Controls.Add(textBox3);
+            Controls.Add(Dob);
+            Controls.Add(lname);
+            Controls.Add(fname);
             FormBorderStyle = FormBorderStyle.None;
-            Name = "FrmAddTrainer";
+            Margin = new Padding(2);
+            Name = "O_AddTrainer";
             Text = "FrmAddTrainer";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox fname;
+        private TextBox lname;
+        private TextBox Dob;
+        private TextBox textBox3;
+        private ComboBox comboBox4;
+        private TextBox textBox4;
+        private TextBox txtusername;
+        private TextBox txtconfirmpass;
+        private TextBox txtpass;
+        private Label label2;
+        private Label label1;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Button DeleteB;
     }
 }
