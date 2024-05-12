4510cff02c6b946994134462e9e9e4c4ef8ed902
umersanii <iamumersani@gmail.com>
2024-05-13 00:38:18 +0500

    0.1.4
    Member Reports (5 Reports)

--- Gym/O_MemberReport.Designer.cs
+++ Gym/O_MemberReport.Designer.cs
@@ -30,6 +30,11 @@
         {
             Refresh = new Button();
             DataView = new DataGridView();
+            comboBox1 = new ComboBox();
+            lname = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            textBox1 = new TextBox();
             ((System.ComponentModel.ISupportInitialize)DataView).BeginInit();
             SuspendLayout();
             // 
@@ -39,12 +44,12 @@
             Refresh.Cursor = Cursors.Hand;
             Refresh.FlatStyle = FlatStyle.Flat;
             Refresh.ForeColor = Color.Gold;
-            Refresh.Location = new Point(532, 623);
+            Refresh.Location = new Point(941, 626);
             Refresh.Margin = new Padding(2);
             Refresh.Name = "Refresh";
             Refresh.Size = new Size(132, 43);
             Refresh.TabIndex = 64;
-            Refresh.Text = "Refresh";
+            Refresh.Text = "Generate";
             Refresh.UseVisualStyleBackColor = false;
             Refresh.Click += Refresh_Click;
             // 
@@ -58,12 +63,71 @@
             DataView.Size = new Size(973, 533);
             DataView.TabIndex = 65;
             // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Details of members of one specific gym that get training from 1 specific trainer.", "Details of members from one specific gym that follow a specific diet plan.", "Details of members across all gyms of a specific trainer that follow a specific diet plan.", "New membership data in last 3 months (Gym Owner).", "Comparison of total members in multiple gyms, in the past 6 months." });
+            comboBox1.Location = new Point(100, 626);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(546, 23);
+            comboBox1.TabIndex = 66;
+            // 
+            // lname
+            // 
+            lname.BackColor = Color.FromArgb(255, 255, 192);
+            lname.BorderStyle = BorderStyle.None;
+            lname.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lname.Location = new Point(822, 627);
+            lname.Margin = new Padding(2);
+            lname.Multiline = true;
+            lname.Name = "lname";
+            lname.Size = new Size(89, 28);
+            lname.TabIndex = 95;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 8F);
+            label2.Location = new Point(704, 666);
+            label2.Name = "label2";
+            label2.Size = new Size(188, 13);
+            label2.TabIndex = 97;
+            label2.Text = "Enter Input Related to Query. Leave";
+
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 8F);
+            label1.Location = new Point(743, 683);
+            label1.Name = "label1";
+            label1.Size = new Size(87, 13);
+            label1.TabIndex = 98;
+            label1.Text = "Blank if not any";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.FromArgb(255, 255, 192);
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(689, 627);
+            textBox1.Margin = new Padding(2);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(94, 28);
+            textBox1.TabIndex = 99;
+            // 
             // O_MemberReport
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Gold;
             ClientSize = new Size(1164, 700);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(label2);
+            Controls.Add(lname);
+            Controls.Add(comboBox1);
             Controls.Add(DataView);
             Controls.Add(Refresh);
             FormBorderStyle = FormBorderStyle.None;
@@ -72,11 +136,17 @@
             Text = "MemberReportF";
             ((System.ComponentModel.ISupportInitialize)DataView).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button Refresh;
         private DataGridView DataView;
+        private ComboBox comboBox1;
+        private TextBox lname;
+        private Label label2;
+        private Label label1;
+        private TextBox textBox1;
     }
 }
--- Gym/O_MemberReport.cs
+++ Gym/O_MemberReport.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Gym
 {
@@ -28,20 +29,182 @@ namespace Gym
             using (SqlConnection con = new SqlConnection("Data Source=SANI-PC\\SQLEXPRESS;Initial Catalog=FLEXTRAINER;Integrated Security=True;Trust Server Certificate=True"))
             {
                 con.Open();
-                SqlDataAdapter sda = new SqlDataAdapter("SELECT * FROM FlexTrainerDataMemberCSV", con);
-                DataTable dt = new DataTable();
-                sda.Fill(dt);
-                DataView.DataSource = dt;
-                dt.Columns.Remove("password");
-                // Change the data display color
-                DataView.DefaultCellStyle.ForeColor = Color.Goldenrod; // Change it to your desired color
-                DataView.DefaultCellStyle.BackColor = Color.Black; // Change it to your desired color
+                string option = comboBox1.SelectedItem?.ToString();
+                string input2 = lname.Text.Trim();
+                string input1 = textBox1.Text.Trim();
+                if (option == "Details of members of one specific gym that get training from 1 specific trainer.")
+                {
+                    string query = @"SELECT FlexTrainerDataMemberCSV.MemberID, FlexTrainerDataMemberCSV.FirstName, FlexTrainerDataMemberCSV.LastName, FlexTrainerDataMemberCSV.DateOfBirth, FlexTrainerDataMemberCSV.Weight, FlexTrainerDataMemberCSV.Height, FlexTrainerDataMemberCSV.Gender,
+                        FlexTrainerDataMemberCSV.Experience_Level, FlexTrainerDataMemberCSV.Goal, FlexTrainerDataMemberCSV.Registeration_Date
+                        FROM FlexTrainerDataGymCSV 
+                        JOIN FlexTrainerDataTrainer_GymCSV ON FlexTrainerDataTrainer_GymCSV.GymID = FlexTrainerDataGymCSV.GymID 
+                        JOIN FlexTrainerDataTrainerCSV ON FlexTrainerDataTrainer_GymCSV.TrainerID = FlexTrainerDataTrainerCSV.TrainerID 
+                        JOIN FlexTrainerDataMember_GymCSV ON FlexTrainerDataTrainer_GymCSV.GymID = FlexTrainerDataMember_GymCSV.GymID 
+                        JOIN FlexTrainerDataMemberCSV ON FlexTrainerDataMemberCSV.MemberID = FlexTrainerDataMember_GymCSV.MemberID 
+                        WHERE Name = @input1 AND FlexTrainerDataTrainerCSV.FirstName = @input2";
 
-                DataView.ColumnHeadersDefaultCellStyle.ForeColor = Color.BlueViolet; // Change it to your desired color
-                DataView.ColumnHeadersDefaultCellStyle.BackColor = Color.Black; // Change it to your desired color
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@input1", input1);
+                        cmd.Parameters.AddWithValue("@input2", input2);
 
-                DataView.GridColor = Color.Black; // Change it to your desired color
+                        SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                        DataTable dt = new DataTable();
+                        sda.Fill(dt);
+
+                        // Check and remove the password column
+                        if (dt.Columns.Contains("password"))
+                            dt.Columns.Remove("password");
+
+                        // Change the data display color
+                        DataView.DataSource = dt;
+                        DataView.DefaultCellStyle.ForeColor = Color.Goldenrod; // Change it to your desired color
+                        DataView.DefaultCellStyle.BackColor = Color.Black; // Change it to your desired color
+                        DataView.ColumnHeadersDefaultCellStyle.ForeColor = Color.BlueViolet; // Change it to your desired color
+                        DataView.ColumnHeadersDefaultCellStyle.BackColor = Color.Black; // Change it to your desired color
+                        DataView.GridColor = Color.Black; // Change it to your desired color
+                    }
+                }
+
+
+
+
+
+                else if (option == "Details of members from one specific gym that follow a specific diet plan.")
+                {
+                    string query = @"SELECT FlexTrainerDataMemberCSV.MemberID, FlexTrainerDataMemberCSV.FirstName, FlexTrainerDataMemberCSV.LastName, FlexTrainerDataMemberCSV.DateOfBirth, FlexTrainerDataMemberCSV.Weight, FlexTrainerDataMemberCSV.Height, FlexTrainerDataMemberCSV.Gender,
+                        FROM FlexTrainerDataGymCSV AS Gym
+                        JOIN FlexTrainerDataMember_GymCSV AS MemberGym ON Gym.GymID = MemberGym.GymID 
+                        JOIN FlexTrainerDataMemberCSV AS Member ON Member.MemberID = MemberGym.MemberID
+						join FlexTrainerDataDietPlanCSV on FlexTrainerDataDietPlanCSV.TrainerID = TrainerID
+						join FlexTrainerDataMember_DietPlanCSV on FlexTrainerDataMember_DietPlanCSV.DPlanID = FlexTrainerDataDietPlanCSV.DPlanID
+                        WHERE Gym.Name = @input1 AND FlexTrainerDataDietPlanCSV.Type = @input2";
+
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@input1", input1);
+                        cmd.Parameters.AddWithValue("@input2", input2);
+
+                        SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                        DataTable dt = new DataTable();
+                        sda.Fill(dt);
+
+                        // Check and remove the password column
+                        if (dt.Columns.Contains("password"))
+                            dt.Columns.Remove("password");
+
+                        // Change the data display color
+                        DataView.DataSource = dt;
+                        DataView.DefaultCellStyle.ForeColor = Color.Goldenrod; // Change it to your desired color
+                        DataView.DefaultCellStyle.BackColor = Color.Black; // Change it to your desired color
+                        DataView.ColumnHeadersDefaultCellStyle.ForeColor = Color.BlueViolet; // Change it to your desired color
+                        DataView.ColumnHeadersDefaultCellStyle.BackColor = Color.Black; // Change it to your desired color
+                        DataView.GridColor = Color.Black; // Change it to your desired color
+                    }
+
+
+                }
+
+                else if (option == "Details of members across all gyms of a specific trainer that follow a specific diet plan.")
+                {
+                    string query = @"SELECT Distinct Member.MemberID, Member.FirstName, Member.LastName, Member.DateOfBirth, Member.Weight, Member.Height, Member.Gender
+                        FROM FlexTrainerDataGymCSV AS Gym
+                        JOIN FlexTrainerDataMember_GymCSV AS MemberGym ON Gym.GymID = MemberGym.GymID 
+                        JOIN FlexTrainerDataMemberCSV AS Member ON Member.MemberID = MemberGym.MemberID
+						JOIN FlexTrainerDataTrainer_GymCSV AS TrainerGym ON Gym.GymID = TrainerGym.GymID
+						JOIN FlexTrainerDataTrainerCSV AS Trainer ON Trainer.TrainerID = TrainerGym.TrainerID
+						join FlexTrainerDataMember_GymCSV on FlexTrainerDataMember_GymCSV.GymID = Gym.GymID
+						join FlexTrainerDataMemberCSV on FlexTrainerDataMemberCSV.MemberID = FlexTrainerDataMember_GymCSV.MemberID
+						join FlexTrainerDataMember_DietPlanCSV on FlexTrainerDataMember_DietPlanCSV.MemberID = FlexTrainerDataMember_GymCSV.MemberID
+						join FlexTrainerDataDietPlanCSV on FlexTrainerDataMember_DietPlanCSV.DPlanID = FlexTrainerDataDietPlanCSV.DPlanID
+                        WHERE Gym.Name = @input1 AND FlexTrainerDataDietPlanCSV.Type = @input2";
+
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@input1", input1);
+                        cmd.Parameters.AddWithValue("@input2", input2);
+
+                        SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                        DataTable dt = new DataTable();
+                        sda.Fill(dt);
+
+                        // Check and remove the password column
+                        if (dt.Columns.Contains("password"))
+                            dt.Columns.Remove("password");
+
+                        // Change the data display color
+                        DataView.DataSource = dt;
+                        DataView.DefaultCellStyle.ForeColor = Color.Goldenrod; // Change it to your desired color
+                        DataView.DefaultCellStyle.BackColor = Color.Black; // Change it to your desired color
+                        DataView.ColumnHeadersDefaultCellStyle.ForeColor = Color.BlueViolet; // Change it to your desired color
+                        DataView.ColumnHeadersDefaultCellStyle.BackColor = Color.Black; // Change it to your desired color
+                        DataView.GridColor = Color.Black; // Change it to your desired color
+                    }
+                }
+
+                else if (option == "New membership data in last 3 months (Gym Owner).")
+                {
+                    string query = @"SELECT DISTINCT Member.MemberID, Member.FirstName, Member.LastName, Member.DateOfBirth, Member.Weight, Member.Height, Member.Gender
+                    FROM FlexTrainerDataGymCSV AS Gym
+                    JOIN FlexTrainerDataMember_GymCSV AS MemberGym ON Gym.GymID = MemberGym.GymID 
+                    JOIN FlexTrainerDataMemberCSV AS Member ON Member.MemberID = MemberGym.MemberID
+                    join FlexTrainerDataOwner_GymCSV on FlexTrainerDataOwner_GymCSV.GymID = Gym.GymID
+                    join FlexTrainerDataOwnerCSV on FlexTrainerDataOwnerCSV.OwnerID = FlexTrainerDataOwner_GymCSV.OwnerID
+                    WHERE FlexTrainerDataOwnerCSV.FirstName = @input1 AND Member.Registeration_Date >= DATEADD(month, -3, GETDATE());";
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@input1", input1);
+                        SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                        DataTable dt = new DataTable();
+                        sda.Fill(dt);
+
+                    
+
+                        // Change the data display color
+                        DataView.DataSource = dt;
+                        DataView.DefaultCellStyle.ForeColor = Color.Goldenrod; // Change it to your desired color
+                        DataView.DefaultCellStyle.BackColor = Color.Black; // Change it to your desired color
+                        DataView.ColumnHeadersDefaultCellStyle.ForeColor = Color.BlueViolet; // Change it to your desired color
+                        DataView.ColumnHeadersDefaultCellStyle.BackColor = Color.Black; // Change it to your desired color
+                        DataView.GridColor = Color.Black; // Change it to your desired color
+                    }
+                }
+
+                else if (option == "Comparison of total members in multiple gyms, in the past 6 months.")
+                {
+                   
+                        string query = @"SELECT Gym.Name AS GymName, COUNT(DISTINCT Member.MemberID) AS TotalMembers
+                        FROM FlexTrainerDataGymCSV AS Gym
+                        JOIN FlexTrainerDataMember_GymCSV AS MemberGym ON Gym.GymID = MemberGym.GymID 
+                        JOIN FlexTrainerDataMemberCSV AS Member ON Member.MemberID = MemberGym.MemberID
+                        WHERE MemberGym.Registeration_Date >= DATEADD(month, -6, GETDATE())
+                        GROUP BY Gym.Name";
+
+                        using (SqlCommand cmd = new SqlCommand(query, con))
+                        {
+                            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                            DataTable dt = new DataTable();
+                            sda.Fill(dt);
+
+                            // Change column names for better representation
+                            dt.Columns["GymName"].ColumnName = "Gym Name";
+                            dt.Columns["TotalMembers"].ColumnName = "Total Members";
+
+                            // Change the data display color
+                            DataView.DataSource = dt;
+                            DataView.DefaultCellStyle.ForeColor = Color.Goldenrod; // Change it to your desired color
+                            DataView.DefaultCellStyle.BackColor = Color.Black; // Change it to your desired color
+                            DataView.ColumnHeadersDefaultCellStyle.ForeColor = Color.BlueViolet; // Change it to your desired color
+                            DataView.ColumnHeadersDefaultCellStyle.BackColor = Color.Black; // Change it to your desired color
+                            DataView.GridColor = Color.Black; // Change it to your desired color
+                        }
+                    
+
+
+                }
             }
         }
+
+
     }
 }
